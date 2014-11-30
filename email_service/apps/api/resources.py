793416876892da0d5ef201ae603b8b5cb72e5755
Pavitra Bhalla <pavitrabhalla@gmail.com>
2014-11-30 05:48:12 +0000

    1. Email formatting method added to views.py. 2. Attachments and fie type handling 3. Sending email via AWS

--- email_service/apps/api/resources.py
+++ email_service/apps/api/resources.py
@@ -3,6 +3,7 @@ from django.conf import settings
 from django.conf.urls import url
 from tastypie.resources import ModelResource, ALL
 from tastypie.http import *
+from api.views import create_formatted_email_msg, send_email_aws
 
 class EmailResource(ModelResource):
     class Meta:
@@ -15,71 +16,45 @@ class EmailResource(ModelResource):
 
     def send_email(self, request, **kwargs):
         self.method_check(request, allowed=['post'])
-	
-	try:
-	    data = json.loads(request.body)
-	except ValueError, e:
-	    data = request.POST
-	except:
-	    traceback.print_exc(FILE = sys.stdout)
-	    return self.create_response(request, {"success":False, "message":"Error: Invalid request body."}, HttpBadRequest)
-			
-	from_address = data.get('from_address')
-	to_addresses = data.get('to_addresses')
-	subject = data.get('subject')
-	body = data.get('body')
-	
-	print "From Address: {}".format(from_address)
-	print "To Addresses: {}".format(to_addresses)
-	print "subject: {}".format(subject)
-	print "body: {}".format(body)
-	
-	for k,v in request.FILES.items():
-	    print k
 
-	import email
-	import boto 
-	import boto.exception 
-	from boto.ses.connection import SESConnection 
-	from django.utils.html import strip_tags
-	from django.template import loader
-	
-	m = email.mime.multipart.MIMEMultipart() 
-	m['Subject'] = subject
-	
-	if from_address:  
-	    m['From'] = from_address
-	else:
-	    m['From'] = settings.DEFAULT_FROM_EMAIL
-	
-	to_array = to_addresses.split(' ')
+        try:
+            data = json.loads(request.body)
+        except ValueError, e:
+            data = request.POST
+        except:
+            traceback.print_exc(file=sys.stdout)
+            return self.create_response(request, {"success":False, "message":"Invalid request body"}, HttpBadRequest)
 
-	#Message body 
-	part = email.mime.text.MIMEText(body) 
-	m.attach(part) 
+        try:
+            from_address = data.get('from_address')
+            if not from_address:
+                return self.create_response(request, {"success":False, "message":"Empty or invalid from_address"}, HttpForbidden)
 
-	#Attachment 
-	#part = email.mime.text.MIMEText('contents of test file here') 
-	#part.add_header('Content-Disposition', 'attachment; filename=test.txt') 
-	#m.attach(part) 
+            to_addresses = data.get('to_addresses')
+            if to_addresses:
+                to_array = to_addresses.split(' ')
+            else:
+                return self.create_response(request, {"success":False, "message":"Empty or invalid to_addresses"}, HttpForbidden)
 
-	try:
-	    conn = boto.ses.connect_to_region(settings.AWS_SES_REGION, aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
-	except:
-	    traceback.print_exc(file=sys.stdout)
+            subject = data.get('subject')
+            body = data.get('body')
+        except:
+            traceback.print_exc(file=sys.stdout)
 
-	r = conn.send_raw_email(source=m['From'], raw_message=m.as_string(), 
-	destinations=to_array)
+        if request.FILES.items():
+            attachments = request.FILES.items()
+        else:
+            attachments = []
 
-        return self.create_response(request, {"status":"Reached here"}, HttpAccepted)
+        content = create_formatted_email_msg(subject, body, attachments)
 
+        try:
+            email_sent = send_email_aws(from_address, to_array, content)
+            if email_sent:
+                return self.create_response(request, {"success":True, "message":"Email sent to recipients"}, HttpAccepted)
+            else:
+                return self.create_response(request, {"success":False, "message":"Internal Error in sending email"}, HttpForbidden)
+        except:
+            traceback.print_exc(file=sys.stdout)
+            return self.create_response(request, {"success":False, "message":"Internal Error in sending email"}, HttpForbidden)
 
-
-
-
-
-
-
-
-
- 
--- email_service/apps/api/views.py
+++ email_service/apps/api/views.py
@@ -1,3 +1,70 @@
+#This file includes helper methods for the APIs
 from django.shortcuts import render
+import email
+import mimetypes
+from email import encoders
+from email.mime.audio import MIMEAudio
+from email.mime.image import MIMEImage
+from email.mime.base import MIMEBase
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+import boto 
+import boto.exception 
+from boto.ses.connection import SESConnection 
+from django.utils.html import strip_tags
+from django.template import loader
+from django.conf import settings
 
-# Create your views here.
+def create_formatted_email_msg(subject, body, attachments):
+    m = MIMEMultipart() 
+
+    #Email subject
+    m['Subject'] = subject
+
+    #Email text/html body
+    part = MIMEText(body, 'html') 
+    m.attach(part)
+
+    #Attachments
+    for k,v in attachments:
+        attached_file = v
+        ctype, encoding = mimetypes.guess_type(attached_file.name)
+        if ctype is None or encoding is not None:
+            # No guess could be made, or the file is encoded (compressed), so
+            # use a generic bag-of-bits type.
+            ctype = 'application/octet-stream'
+        maintype, subtype = ctype.split('/', 1)
+
+        if maintype == 'text':
+            # Note: we should handle calculating the charset
+            msg = MIMEText(attached_file.read(), _subtype=subtype)
+
+        elif maintype == 'image': 
+            msg = MIMEImage(attached_file.read(), _subtype=subtype)
+
+        elif maintype == 'audio': 
+            msg = MIMEAudio(attched_file.read(), _subtype=subtype)
+
+        elif maintype == 'application':
+            msg = MIMEApplication(attached_file.read(), _subtype=subtype)
+
+        else: 
+            msg = MIMEBase(maintype, subtype)
+            msg.set_payload(attached_file.read())
+            # Encode the payload using Base64
+            encoders.encode_base64(msg)
+        # Set the filename parameter
+        msg.add_header('Content-Disposition', 'attachment', filename=k)
+        m.attach(msg)
+    return m
+
+def send_email_aws(from_address, to_addresses, content):
+    try:
+        conn = boto.ses.connect_to_region(settings.AWS_SES_REGION, aws_access_key_id=settings.AWS_ACCESS_KEY_ID, aws_secret_access_key=settings.AWS_SECRET_ACCESS_KEY)
+    except:
+        traceback.print_exc(file=sys.stdout)
+
+    r = conn.send_raw_email(source=from_address, raw_message=content.as_string(), destinations=to_addresses)
+    return True
+
